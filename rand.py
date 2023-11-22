0b3d11d49d9d3dee62f60c26af85fe0b4b4dc1f8
DANIEL MUNENE IKUA <123269657+TheCode69@users.noreply.github.com>
2023-11-22 19:39:33 +0300

    Add files via upload
    first commit

---
+++ rand.py
@@ -0,0 +1,9 @@
+import random
+
+# Generate a random integer between a specified range (inclusive)
+random_number = random.randint(1, 100)
+print("Random Number:", random_number)
+
+# Generate a random floating-point number between 0 and 1
+random_float = random.random()
+print("Random Float:", random_float)
